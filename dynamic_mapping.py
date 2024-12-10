f976bd526e8f3c582725760da6dbd808eabc4379
Kovuta <Kovuta@users.noreply.github.com>
2024-12-10 11:42:33 -0700

    Adding script to grab numericIDs of NetBox dynamically

---
+++ dynamic_mapping.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+
+# Load keys using environment
+load_dotenv()
+
+net_url = os.getenv("NETBOX_URL")
+net_token = os.getenv("NETBOX_TOKEN")
+headers = {"Authorization:" f"Token {net_token}"}
+
+def get_mapping(path, key_field, value_field):
+    url = f"{net_url}{path}"
+    response = requests.get(url, headers)
+    response.raise_for_status()
+    data = response.json()["results"]
+    return {item[key_field]: item[value_field] for item in data}
+
+
+# Calling API to grab proper mappings
+mappings = {
+    "device_type_mapping": fetch_mapping("dcim/device-types/", "model", "id"),
+    "site_mapping": fetch_mapping("dcim/sites/", "name", "id"),
+    "tenant_mapping": fetch_mapping("tenancy/tenants/", "name", "id")
+}
+
+with open("mappings.json", "w") as f:
+    json.dump(mappings, f, indent=4)
+print("Mappings have been saved to mappings.json")
